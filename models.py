6804a5efb96acfe6e6398a9e576e8bac55fc2e57
Joshua Lokken <jrrlokken@gmail.com>
2020-06-29 13:00:16 -0500

    Added post model

--- models.py
+++ models.py
@@ -30,3 +30,16 @@ class User(db.Model):
 
         u = self
         return f"<User id={u.id} name={u.first_name} {u.last_name}>"
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False,
+                           default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
