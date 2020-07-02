aabf479757bc410acfbf6944cd0b63a5e35dc3e3
Joshua Lokken <jrrlokken@gmail.com>
2020-07-01 20:34:35 -0500

    Removed nullable where not needed

--- models.py
+++ models.py
@@ -61,6 +61,6 @@ class PostTag(db.Model):
     __tablename__ = 'posts_tags'
 
     post_id = db.Column(db.Integer, db.ForeignKey(
-        'posts.id'), primary_key=True, nullable=False)
+        'posts.id'), primary_key=True)
     tag_id = db.Column(db.Integer, db.ForeignKey(
-        'tags.id'), primary_key=True, nullable=False)
+        'tags.id'), primary_key=True)
